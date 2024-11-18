d6cf68180c8154bb817fb036a678f92af76e356c
ENZOGADDI-STUDENT <G00431922@atu.ie>
2024-11-18 09:35:43 +0000

    Task 1 for inheritance lab

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = scan.nextLine();
+
+        System.out.println("Please enter your address: ");
+        String address = scan.nextLine();
+
+        System.out.println("Please enter your phone: ");
+        String phone = scan.nextLine();
+
+        System.out.println("Please enter your customer number: ");
+        String custNum = scan.nextLine();
+
+        System.out.println("Do you want to be on the mailing list: ");
+        boolean list = scan.hasNextBoolean();
+
+        Customer newCust = new Customer(name, address, phone, custNum, list);
+        System.out.println("You entered " + newCust.toString());
+    }
+}
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,26 @@
 public class Person
 {
-    private
+    private String name;
+    private String address;
+    private String phone;
+
+    public Person()
+    {
+        this.name = "";
+        this.address = "";
+        this.phone = "";
+    }
+    public Person(String name, String address, String phone)
+    {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' ;
+    }
 }
